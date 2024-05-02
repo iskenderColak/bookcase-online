ab5a92365d65c26c9bce1d477d0c2aca49cf0852
iskender <iclk84@gmail.com>
2024-05-03 01:03:25 +0300

    Book Service GeneralExceptionHandler @RestControllerAdvice

---
+++ book-service/src/main/java/bookservice/exception/GeneralExceptionHandler.java
@@ -0,0 +1,15 @@
+package bookservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GeneralExceptionHandler {
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<?> handle(BookNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
